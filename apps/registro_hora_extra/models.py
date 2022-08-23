9b5891fbdc1e2cf23969be5266a921034567152e
Renata Helena Pereira Negrão <rpnegrao@outlook.com>
2022-08-23 17:04:03 -0300

    Adicionando arquivos deploy

--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 from apps.funcionarios.models import Funcionario
 
 class RegistroHoraExtra(models.Model):
@@ -6,5 +8,9 @@ class RegistroHoraExtra(models.Model):
     funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
     horas = models.DecimalField(max_digits=5, decimal_places=2)
 
+    def get_absolute_url(self):
+        return reverse('update_hora_extra', args=[self.id])
+
     def __str__(self):
         return self.motivo
+
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,3 +1,6 @@
+from django.urls import reverse_lazy
+
+from .forms import RegistroHoraExtraForm
 from .models import RegistroHoraExtra
 from django.views.generic import (
     ListView,
@@ -12,3 +15,23 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
+
+class HoraExtraEdit(UpdateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'funcionario', 'horas']
+
+class HoraExtraDelete(DeleteView):
+    model = RegistroHoraExtra
+    success_url = reverse_lazy('list_hora_extra')
+
+class HoraExtraNovo(CreateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraNovo, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
+
