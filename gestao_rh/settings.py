42a50b5063091be942c99cc05f12d2edac4c4a3a
Renata Helena Pereira Negrão <rpnegrao@outlook.com>
2022-08-29 21:02:06 -0300

    Alterando o IP

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wea_()#9!jgzl$esmubusr@#1h$jp8=f=(1-m&xfzwc$-s+2t6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.87.93.62']
+ALLOWED_HOSTS = ['52.72.68.71']
 
 
 # Application definition
