93cf3436d9413e84a66ad40385c3b41d72a730b3
Renata Helena Pereira Negrão <rpnegrao@outlook.com>
2022-10-02 18:30:59 -0300

    Alteracoes Settings

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wea_()#9!jgzl$esmubusr@#1h$jp8=f=(1-m&xfzwc$-s+2t6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.91.249.123']
+ALLOWED_HOSTS = ['3.91.249.123', '127.0.0.1']
 #ALLOWED_HOSTS = ['127.0.0.1']
 
 
@@ -84,12 +84,6 @@ WSGI_APPLICATION = 'gestao_rh.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 #STATICFILES_DIRS = [
 #    BASE_DIR / "static",
@@ -158,4 +152,4 @@ LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'login'
 
-
+from .local_settings import *
